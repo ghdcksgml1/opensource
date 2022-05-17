e51ed4bbf6dcd0e2ff6a27c569fe36207af504e5
ghdcksgml1 <ghdcksgml2@naver.com>
2022-05-17 17:45:30 +0900

    add: 프로젝트 생성

---
+++ src/test/java/opensource/algorhythm/AlgorhythmApplicationTests.java
@@ -0,0 +1,13 @@
+package opensource.algorhythm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorhythmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
