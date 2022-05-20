a9c1e8a215fd088461a37f62c8ed307561858e99
kys0411 <32180846@dankook.ac.kr>
2022-05-21 01:19:15 +0900

    add : 회원 가입 화면으로부터 넘어오는 정보를 저장하는 dto 추가

---
+++ src/main/java/opensource/algorhythm/dto/MemberFormDto.java
@@ -0,0 +1,14 @@
+package opensource.algorhythm.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MemberFormDto {
+
+    private String name;
+
+    private String email;
+
+    private String password;
+}
