1351580ae1e7839d78f222630ef09e7561aa2a5d
kys0411 <32180846@dankook.ac.kr>
2022-05-21 01:26:26 +0900

    feat : memberService에 회원가입 기능 추가

--- src/main/java/opensource/algorhythm/service/MemberService.java
+++ src/main/java/opensource/algorhythm/service/MemberService.java
@@ -1,2 +1,27 @@
-package opensource.algorhythm.service;public class MemberService {
+package opensource.algorhythm.service;
+
+import lombok.RequiredArgsConstructor;
+import opensource.algorhythm.entity.Member;
+import opensource.algorhythm.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    private void validateDuplicateMember(Member member){
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+        if (findMember != null){
+            throw  new IllegalStateException("이미 가입된 회원입니다");
+        }
+    }
+
+    public Member saveMember(Member member){
+        validateDuplicateMember(member);
+        return memberRepository.save(member);
+    }
 }
