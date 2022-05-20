7e6e682ad8f77c0b7490ea9a873fad5ae9d95fff
kys0411 <32180846@dankook.ac.kr>
2022-05-21 01:09:41 +0900

    add : member entity에 필드 추가

--- src/main/java/opensource/algorhythm/entity/Member.java
+++ src/main/java/opensource/algorhythm/entity/Member.java
@@ -1,4 +1,28 @@
 package opensource.algorhythm.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Getter @Setter
+@ToString
 public class Member {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+
 }
