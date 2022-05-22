dc3affa10ad13189a2dbb589709f562496e51d85
kys0411 <32180846@dankook.ac.kr>
2022-05-22 13:21:07 +0900

    add : MemberService가 UserDetailService 인터페이스를 구현, 메서드 오버라이딩

--- src/main/java/opensource/algorhythm/service/MemberService.java
+++ src/main/java/opensource/algorhythm/service/MemberService.java
@@ -3,13 +3,17 @@ package opensource.algorhythm.service;
 import lombok.RequiredArgsConstructor;
 import opensource.algorhythm.entity.Member;
 import opensource.algorhythm.repository.MemberRepository;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
@@ -23,5 +27,20 @@ public class MemberService {
     public Member saveMember(Member member){
         validateDuplicateMember(member);
         return memberRepository.save(member);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Member member = memberRepository.findByEmail(email);
+
+        if (member == null){
+            throw new UsernameNotFoundException(email);
+        }
+
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
     }
 }
