695a122a55dc17f21a97c78e327bd9a111c3e9b4
kys0411 <32180846@dankook.ac.kr>
2022-05-22 13:09:08 +0900

    refactor : 회원가입 메서드에 validation 코드 추가

--- src/main/java/opensource/algorhythm/controller/MemberController.java
+++ src/main/java/opensource/algorhythm/controller/MemberController.java
@@ -7,9 +7,12 @@ import opensource.algorhythm.service.MemberService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequestMapping("/members")
 @Controller
@@ -26,10 +29,21 @@ public class MemberController {
     }
 
     @PostMapping(value = "/new")
-    public String memberForm(MemberFormDto memberFormDto){
-        Member member = Member.createMember(memberFormDto, passwordEncoder);
-        memberService.saveMember(member);
+    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
+
+        if (bindingResult.hasErrors()){
+            return "signup";
+        }
+
+        try {
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        }catch (IllegalStateException e){
+            model.addAttribute("errorMessage", e.getMessage());
+            return "signup";
+        }
 
         return "redirect:/";
     }
+
 }
