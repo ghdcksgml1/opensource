79780a166e4772621e2a6862711dae9c062908a7
kys0411 <32180846@dankook.ac.kr>
2022-06-08 02:18:32 +0900

    add : post 수정에 필요한 dto 추가

--- src/main/java/opensource/algorhythm/dto/PostEditDto.java
+++ src/main/java/opensource/algorhythm/dto/PostEditDto.java
@@ -1,2 +1,14 @@
-package opensource.algorhythm.dto;public class PostEditDto {
+package opensource.algorhythm.dto;
+
+import lombok.Getter;
+
+@Getter
+public class PostEditDto {
+    private String title;
+
+    private String problemIdea;
+
+    private String timeComplexity;
+
+    private String code;
 }
