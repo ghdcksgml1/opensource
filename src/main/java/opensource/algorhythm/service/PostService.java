523b0987c00b30a18385676f830421675d6c58be
kys0411 <32180846@dankook.ac.kr>
2022-06-08 02:19:02 +0900

    feat : post crud, search 기능 추가

--- src/main/java/opensource/algorhythm/service/PostService.java
+++ src/main/java/opensource/algorhythm/service/PostService.java
@@ -1,12 +1,14 @@
 package opensource.algorhythm.service;
 
 import lombok.RequiredArgsConstructor;
+import opensource.algorhythm.dto.PostEditDto;
 import opensource.algorhythm.dto.PostFormDto;
 import opensource.algorhythm.entity.Post;
 import opensource.algorhythm.repository.PostRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,6 +21,7 @@ public class PostService {
 
     //post 생성
     public Post createPost(PostFormDto postFormDto){
+
         Post post = Post.createPost(postFormDto);
         return postRepository.save(post);
     }
@@ -29,34 +32,49 @@ public class PostService {
         return post;
     }
 
+    //모든 post 조회
     public List<Post> findAllPost(){
         return postRepository.findAll();
     }
 
     //post 수정
-    public String editPost(Long id, PostFormDto postFormDto){ //수정한 dto
-        Post post = postRepository.getById(id);
-        post.setTitle(postFormDto.getTitle());
-        post.setCode(postFormDto.getCode());
-        post.setProblemName(postFormDto.getProblemName());
-        post.setProblemNum(postFormDto.getProblemNum());
+    public Post editPost(Long postId, PostEditDto postEditDto){ //수정한 dto
+        Post post = postRepository.findById(postId).get();
 
-        postRepository.save(post);
+        post.setTitle(postEditDto.getTitle());
+        post.setProblemIdea(postEditDto.getProblemIdea());
+        post.setCode(postEditDto.getCode());
+        post.setTimeComplexity(postEditDto.getTimeComplexity());
 
-        return "";
+        return postRepository.save(post);
     }
 
     //post 삭제
-    public String deletePost(Long id){
+    public void deletePost(Long id){
         postRepository.deleteById(id);
-        return "";
     }
 
-    //post 검색
-    public String searchPost(String keyword){
-        List<Post> postListByProblemNum = postRepository.findByProblemNum(Integer.parseInt(keyword));
+    //post 검색 - 문제 번호 or 태그로 검색
+    public List<Post> searchPost(String keyword){
+        List<Post> postList = new ArrayList<>();
+
+        try {
+            List<Post> postListByProblemNum = postRepository.findByProblemNum(Integer.parseInt(keyword.trim()));
+            if (postListByProblemNum != null){
+                for (Post post : postListByProblemNum) {
+                    postList.add(post);
+                }
+            }
+        }catch (NumberFormatException e){
+
+        }
+
         List<Post> postListByTag = postRepository.findByTagsContainingIgnoreCase(keyword);
-        List<Post> postListByProblemName= postRepository.findByProblemNameContaining(keyword);
-        return "ok";
+        if (postListByTag != null){
+            for (Post post : postListByTag) {
+                postList.add(post);
+            }
+        }
+        return postList;
     }
 }
