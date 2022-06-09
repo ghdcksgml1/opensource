c207f9ec3e341d571d4ef5f6aed0959a32283202
kys0411 <32180846@dankook.ac.kr>
2022-06-09 21:37:16 +0900

    refactor : post와 comment 연관관계 수정

--- src/main/java/opensource/algorhythm/entity/Comment.java
+++ src/main/java/opensource/algorhythm/entity/Comment.java
@@ -3,6 +3,8 @@ package opensource.algorhythm.entity;
 import lombok.Getter;
 import lombok.Setter;
 import opensource.algorhythm.dto.CommentDto;
+import opensource.algorhythm.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.persistence.*;
 
@@ -16,18 +18,12 @@ public class Comment {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
-  private Long postId;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "post_id")
+  private Post post;
   
   private Long memberId;
   
   private String content;
-
-  public static Comment createComment(CommentDto commentDto){
-    Comment comment = new Comment();
-    comment.setContent(commentDto.getContent());
-    comment.setPostId(commentDto.getPostId());
-    comment.setMemberId(commentDto.getMemberId());
-    return comment;
-  }
 
 }
