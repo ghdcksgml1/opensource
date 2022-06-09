8e55f1b48774ee2eec3f2d93437b5b8b90a02277
kys0411 <32180846@dankook.ac.kr>
2022-06-10 02:49:46 +0900

    refactor : 메서드 이름(쿼리) 수정

--- src/main/java/opensource/algorhythm/repository/PostRepository.java
+++ src/main/java/opensource/algorhythm/repository/PostRepository.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findByProblemNum(int problemNum);
-    List<Post> findByTagsContainingIgnoreCase(String tag);
     List<Post> findByProblemNameContaining(String name);
+
+    List<Post> findByTagsContaining(String keyword);
 }
