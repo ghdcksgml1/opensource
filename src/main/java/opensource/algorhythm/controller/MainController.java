b4b3ca15ee4e7234a557bbf2edd348bb0f7c0134
kys0411 <32180846@dankook.ac.kr>
2022-06-10 02:49:13 +0900

    refactor : main controller에 게시물 검색 매핑 추가

--- src/main/java/opensource/algorhythm/controller/MainController.java
+++ src/main/java/opensource/algorhythm/controller/MainController.java
@@ -9,7 +9,10 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -32,6 +35,27 @@ public class MainController {
         return "index";
     }
 
+    //게시물 검색
+    @RequestMapping(value = "/search")
+    public String searchPost(@RequestParam String keyword, @RequestParam int problemNum, Model model){
+        List<Post> searchPostList = new ArrayList<>();
 
+        List<Post> postListByTag = postService.searchPostByTag(keyword);
+        List<Post> postListByProblemNum = postService.searchPostByProblemNum(problemNum);
+        log.info("search result: {}", postListByTag.get(0).getTitle());
+        if (postListByTag != null){
+            for (Post post : postListByTag) {
+                searchPostList.add(post);
+            }
+        }
+        if (postListByProblemNum != null){
+            for (Post post : postListByProblemNum) {
+                searchPostList.add(post);
+            }
+        }
+
+        model.addAttribute("postList", searchPostList);
+        return "index";
+    }
 
 }
